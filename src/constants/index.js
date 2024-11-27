270a85522f071bf0f7aabe9b9cf57072930ef68e
Olaniyan Temitope <olaniyantemitope69@gmail.com>
2024-11-27 06:19:49 -0800

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,14 +5,14 @@ import project4 from "../assets/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for 
 crafting robust and scalable web applications. With 3 years of hands-on experience,
- I have honed my skills in front-end technologies like JavaScript, React and Express.js, 
- as well as back-end technologies like Node.js and MongoDB.
+ I have honed my skills in front-end technologies like JavaScript and React, 
+ as well as back-end technologies like Node.js and Express.js. Also MongoDb for Database technologies 
 My goal is to leverage my expertise to create innovative solutions that 
 drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for 
 creating efficient and user-friendly web applications. With 3 years of self-taught experience in project developments, 
-I have worked with a variety of technologies, including React, Express.js, Node.js, MySQL, MongoDb, I also have understanding of syntaxes in C++, Java, and Phyton.
+I have worked with a variety of technologies, including React, Express.js, Node.js, MongoDb, I also have understanding of syntaxes in C++, Java, and Phyton.
 My journey in web development began with a deep curiosity for how things work, and it has 
 evolved into a career where I continuously strive to learn and adapt to new challenges. 
 I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions.
