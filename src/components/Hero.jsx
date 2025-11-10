e54516072db7eda5b0d4699afc22abd65413ebbf
olat69 <olaniyantemitope69@gmail.com>
2025-11-10 14:24:28 -0700

    ...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-12 text-6xl font-thin tracking-tight lg:mt-5 lg:text-6xl"
+              className="pb-10 text-6xl font-thin tracking-tight lg:mt-5 lg:text-6xl"
             >
               Olaniyan Temitope
             </motion.h1>
@@ -25,7 +25,7 @@ const Hero = () => {
               variants={container(0.5)}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
+              className="w-full pb-5 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
             bg-clip-text text-4xl tracking-tight text-transparent"
             >
               Software Engineer
